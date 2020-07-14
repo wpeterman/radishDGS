b1079467dae510491bc9314297082f0f927abb0d
Peterman <peterman.73@osu.edu>
2020-07-14 15:32:43 -0400

    Functions added by WEP to run CDPOP simulation

---
+++ R/bin2cont_function.R
@@ -0,0 +1,16 @@
+#' @author Bill Peterman
+#' @description Function to create a continuous surface from a binary surface
+#' 
+#' @param bin_rast Binary (1/0) raster
+#' @param window The size of the moving window to calculate mean habitat within (Default = 3)
+#' 
+#' @export
+
+bin2cont <- function(bin_rast,
+                     window = 3){
+  library(raster)
+  
+  cont_rast <- focal(bin_rast,
+                     w = matrix(1 / window^2, nc = window, nr = window))
+  return(cont_rast)
+}
---
+++ R/landscape_sim_function.R
@@ -0,0 +1,47 @@
+#' @author Bill Peterman
+#' @description Function to conduct landscape simulation with NLMR and genetic simulation with CDPOP
+#' 
+#' @param corr Desired correlation level between fbm surfaces
+#' @param dim Dimension of simulated raster (Default = 400)
+#' @param H Fractal dimension (Hurst parameter, H) controlling clustering in Fractal Brownian Motion landscape simulation model (Default = 0.5)
+#' @param user_seed Seed can be set for reproducibility (Default = NULL)
+#' 
+#' @export
+
+fbm_corr <- function(corr,
+                     dim,
+                     H = 0.5,
+                     user_seed = NA){
+  
+  sim_rast <- nlm_fbm(ncol = dim,
+                      nrow = dim, 
+                      fract_dim = H, 
+                      user_seed = user_seed)
+  
+  rast_corr <- 0
+  
+  while(rast_corr > corr + 0.015 | rast_corr < corr - 0.015){
+    rep_sim <- nlm_fbm(ncol = dim,
+                       nrow = dim, 
+                       fract_dim = H, 
+                       user_seed = NA)
+    
+    corr_ <-  sqrt((1 / (corr^2)) - 1) 
+    mat <- as.matrix(sim_rast)
+    
+    rep_sim_ <- rep_sim * corr_
+    corr_rast <- sim_rast + rep_sim_
+    
+    rast_corr <- layerStats(stack(sim_rast, corr_rast), 'pearson')$`pearson correlation coefficient`[1,2]
+    cat(paste0(round(rast_corr, digits = 4), '\n \n'))
+  }
+  
+  cat(paste0("Surface correlation is ", round(rast_corr, 4), '\n \n'))
+  
+  
+  corr_rast[] <- (corr_rast[] - min(corr_rast[])) / (max(corr_rast[]) - min(corr_rast[]))
+  
+  
+  return(stack(list(sim_true = sim_rast,
+                    corr_sim = corr_rast)))
+}
